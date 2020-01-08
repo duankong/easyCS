15c95126634ff35cde519e6e5b62d2d1095aa63d
DK_G211 <lxblearning@outlook.com>
2020-01-08 18:33:54 +0800

    train ESSnet test

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import argparse
 def args_config():
     parser = argparse.ArgumentParser(description='train demo')
     # model
-    parser.add_argument('--model', type=str, default='Unet_conv',
-                        help='the model name：Unet nestedUnet Unet_res Unet_conv')  # [**]
-    parser.add_argument('--model_name', type=str, default='Unet_conv_test.t7', help='the model name')  # [**]
-    parser.add_argument('--model_log', type=str, default='Unet_conv_test', help='the model log file for check')  # [**]
+    parser.add_argument('--model', type=str, default='Essnet',
+                        help='the model name：Unet nestedUnet Unet_res Unet_conv Essnet')  # [**]
+    parser.add_argument('--model_name', type=str, default='Essnet_test2.t7', help='the model name')  # [**]
+    parser.add_argument('--model_log', type=str, default='Essnet_test2', help='the model log file for check')  # [**]
     parser.add_argument('--model_save', type=bool, default=False, help='the model is save or not')
     parser.add_argument('--model_show', type=bool, default=False, help='the model is show  or not')
     parser.add_argument('--img_n_channels', type=int, default=1, help='the input data channels')
@@ -22,7 +22,7 @@ def args_config():
     parser.add_argument('--maskname', type=str, default='poisson2d', help='gaussian1d, gaussian2d, poisson2d')  # [**]
     parser.add_argument('--maskperc', type=int, default=10, help='1,5,10,20,30,40,50')  # [**]
     # loss
-    parser.add_argument('--loss_mse_only', type=bool, default=False, help='Only use Mse as the loss')
+    parser.add_argument('--loss_mse_only', type=bool, default=True, help='Only use Mse as the loss')
     parser.add_argument('--loss_ssim', type=bool, default=True, help='add SSIM into the loss')
     parser.add_argument('--loss_vgg', type=bool, default=True, help='add vggNet into the loss')
     parser.add_argument('--alpha', type=float, default=15, help='alpha = 15')
@@ -30,7 +30,7 @@ def args_config():
     parser.add_argument('--beta', type=float, default=0.0025, help='beta = 0.0025')
     # train config
     parser.add_argument('--epochs', type=int, default=100, help='train epoch ')  # [**]
-    parser.add_argument('--lr', type=float, default=1e-2, help='initial learning rate')  # [**]
+    parser.add_argument('--lr', type=float, default=1e-2,help='initial learning rate')  # [**]
     parser.add_argument('--batch_size', type=int, default=8, help='train batch size ')
     return parser.parse_args()
 
--- utils/define_model.py
+++ utils/define_model.py
@@ -1,16 +1,18 @@
-from model import unet, nestedUnet
+from model import UNet, NestedUNet, UNet_res, UNet_conv, ESS_net
 
 
 def get_model(model_name, n_channels, n_classes):
     global net
     if model_name == "Unet":
-        net = unet.UNet(n_channels=n_channels, n_classes=n_classes)
+        net = UNet(n_channels=n_channels, n_classes=n_classes)
     elif model_name == "nestedUnet":
-        net = nestedUnet.NestedUNet(n_channels=n_channels, n_classes=n_classes, deepsupervision=False)
+        net = NestedUNet(n_channels=n_channels, n_classes=n_classes, deepsupervision=False)
     elif model_name == "Unet_res":
-        net = unet.UNet_res(n_channels=n_channels, n_classes=n_classes)
+        net = UNet_res(n_channels=n_channels, n_classes=n_classes)
     elif model_name == "Unet_conv":
-        net = unet.UNet_conv(n_channels=n_channels, n_classes=n_classes, bilinear=True, Measure_return=False)
+        net = UNet_conv(n_channels=n_channels, n_classes=n_classes, bilinear=True, Measure_return=False)
+    elif model_name == "Essnet":
+        net = ESS_net(in_channels=n_channels, out_channels=n_classes)
     else:
         return 0
     return net
