ce4679ec3301eb1410ec99cfb7049f29dd3b298d
duankong <lxblearning@gmail.com>
2020-04-16 09:21:24 +0800

    update predict code

--- config.py
+++ config.py
@@ -56,16 +56,18 @@ def args_config():
 def args_config_predict():
     parser = argparse.ArgumentParser(description='predict demo')
     # model
-    parser.add_argument('--model', type=str, default='UNet_res',
-                        help='the model name：Unet nestedUnet UNet_res Unet_conv')  # [**]
-    parser.add_argument('--model_name', type=str, default='test_log.t7', help='the model name')  # [**]
-    parser.add_argument('--model_show', type=bool, default=False, help='the model is show  or not')
+    parser.add_argument('--model', type=str, default='Essnet',
+                        choices=['Unet', 'nestedUnet', 'Unet_res', 'Unet_conv', 'Essnet'],
+                        help='the model name：Unet nestedUnet Unet_res Unet_conv Essnet')  # [**]
+    parser.add_argument('--model_name', type=str, default='Essnet_test4.t7', help='the model name')  # [**]
+    parser.add_argument('--model_show', type=boolean_string, default=False, choices=[True, False],
+                        help='the model is show  or not')
     parser.add_argument('--img_n_channels', type=int, default=1, help='the input data channels')
     parser.add_argument('--img_n_classes', type=int, default=1, help='the ouput data classes')
     # test data
-    parser.add_argument('--test_path', type=str, default='./data/17782/', help='data path')
+    parser.add_argument('--test_source_path', type=str, default='./data/17782/', help='data path')
     parser.add_argument('--test_target_path', type=str, default='./data/predict/', help='data save path')
-    parser.add_argument('--test_star_num', type=int, default=1, help='the first pic num')  # [**]
+    parser.add_argument('--test_sta_num', type=int, default=1, help='the first pic num')  # [**]
     parser.add_argument('--test_end_num', type=int, default=10, help='the end pic num')  # [**]
     parser.add_argument('--img_size_x', type=int, default=256, help='the input data size x(row)')
     parser.add_argument('--img_size_y', type=int, default=256, help='the input data size y(col)')
@@ -74,12 +76,14 @@ def args_config_predict():
     parser.add_argument('--Subimg_size_x', type=int, default=256, help='the Subimg_size_x')
     parser.add_argument('--Subimg_size_y', type=int, default=256, help='the Subimg_size_y')
     # mask
-    parser.add_argument('--maskname', type=str, default='poisson2d', help='gaussian1d, gaussian2d, poisson2d')  # [**]
-    parser.add_argument('--maskperc', type=int, default=10, help='1,5,10,20,30,40,50')  # [**]
+    parser.add_argument('--maskname', type=str, default='poisson2d', choices=['gaussian1d', 'gaussian2d', 'poisson2d'],
+                        help='gaussian1d, gaussian2d, poisson2d')  # [**]
+    parser.add_argument('--maskperc', type=int, default=10, choices=[1, 5, 10, 20, 30, 40, 50],
+                        help='1,5,10,20,30,40,50')  # [**]
     # predict config
     parser.add_argument('--test_batch_size', type=int, default=8, help='test batch size ')
     # save
-    parser.add_argument('--save_real', type=bool, default=False, help='save the real')
+    parser.add_argument('--save_real', type=boolean_string, default=False, choices=[True, False], help='save the real')
     return parser.parse_args()
 
 
