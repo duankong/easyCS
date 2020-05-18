32949b1547207a9950823f43bbd19b8fec581bee
duankong <lxblearning@gmail.com>
2020-05-18 18:35:48 +0800

    update 05.18

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@ git status
 echo -e "\033[33m[*] Check done ! \033[0m"
 git add -A
 echo -e "\033[33m[*] Add all ! \033[0m"
-git commit -m "update 05.11"
+git commit -m "update 05.18"
 echo -e "\033[33m[*] Commit ! \033[0m"
 git push
 echo -e "\033[33m[*] Push Done ! \033[0m"
