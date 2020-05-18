4fdcf62f582abf71802de326abe73775948711cf
duankong <lxblearning@gmail.com>
2020-05-18 18:34:59 +0800

    add huffman code

---
+++ update.sh
@@ -0,0 +1,8 @@
+git status
+echo -e "\033[33m[*] Check done ! \033[0m"
+git add -A
+echo -e "\033[33m[*] Add all ! \033[0m"
+git commit -m "update 05.11"
+echo -e "\033[33m[*] Commit ! \033[0m"
+git push
+echo -e "\033[33m[*] Push Done ! \033[0m"
