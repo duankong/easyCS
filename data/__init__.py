febc92c60fd6234b80b14b52e0a5ae3814432932
duankong <lxblearning@gmail.com>
2020-05-22 21:35:04 +0800

    update compression 05 22

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,5 @@
 from .data_block_fun import get_Blocks, assembleBlocks
 from .data_assess import ssim, psnr, mse_value
-from .data_loader import generate_train_test_data, get_test_image,generate_bigimage
+from .data_loader import generate_train_test_data, get_test_image, generate_bigimage
 from .mask_utils import get_mask
+from .huffman import IntlNode, LeafNode, HuffNode, HuffTree, buildHuffmanTree
