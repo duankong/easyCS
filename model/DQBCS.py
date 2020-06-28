4fa8af1db7c355f0411519b3c621d47af4060632
duankong <lxblearning@gmail.com>
2020-06-28 17:04:47 +0800

    update DQBCS

--- model/DQBCS.py
+++ model/DQBCS.py
@@ -5,20 +5,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 from config import args_config
 
-class Sample_subNetwork(nn.Module):
-    def __init__(self):
-        super(Sample_subNetwork,self).__init__()
+B = 32
 
-    def forward(self, *input):
+
+class Sample_subNetwork(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Sample_subNetwork, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=B, stride=B)
+
+    def forward(self, input):
+        x = self.conv(input)
+        return x
+
+class Offset_subNetwork(nn.Module):
+    def __init__(self,in_channels,out_channels):
+        super(Offset_subNetwork,self).__init__()
+
+    def forward(self, input):
         return None
 
 
 class ResBlock(nn.Module):
-    def __init__(self, Nin, out, kernel_size=3, stride=1):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1):
         super(ResBlock, self).__init__()
-        self.conv1 = nn.Conv2d(Nin, out, kernel_size=kernel_size, stride=stride, padding=1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=1)
         self.Relu = nn.ReLU()
-        self.bn = nn.BatchNorm2d(out)
+        self.bn = nn.BatchNorm2d(out_channels)
 
     def forward(self, input):
         x = input
--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@ git status
 echo -e "\033[33m[*] Check done ! \033[0m"
 git add -A
 echo -e "\033[33m[*] Add all ! \033[0m"
-git commit -m "update quantization"
+git commit -m "update DQBCS"
 echo -e "\033[33m[*] Commit ! \033[0m"
 git push
 echo -e "\033[33m[*] Push Done ! \033[0m"
