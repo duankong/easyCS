aeb47aa5698865b4b4cf13c9e628ac3d59e8ddad
duankong <lxblearning@gmail.com>
2020-07-05 09:59:56 +0800

    test DQBCS

--- temp.sh
+++ temp.sh
@@ -6,4 +6,5 @@ D:/anconda3/python.exe -u  G:/lxb/desktop/easyCS/train_DQBCS.py \
 --model_name  DQBCS_v0.t7  --model_log  DQBCS_v0 \
 --maskname poisson2d --maskperc  5  \
 --epochs 60 --lr 1e-3 --batch_size 16 \
---loss_mse_only True --loss_ssim False --loss_vgg False\
+--loss_mse_only True --loss_ssim False --loss_vgg False \
+--DQBCS_rate 0.05\
