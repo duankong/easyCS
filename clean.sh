e2b7f060af63d4318ceba793af32aa394250557a
DK_G211 <lxblearning@outlook.com>
2020-08-23 16:25:16 +0800

    init

--- clean.sh
+++ clean.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 echo -e "\033[33m[*] Start clean ! \033[0m"
-cp -r
+cp -r runs/* bak_runs/
+echo -e "[*] runs done "
+cp -r checkpoint bak_runs/
+echo -e "[*] checkpoint done "
+rm -rf runs/*
+rm -rf checkpoint/*
 echo -e "\033[33m[*] Push Done ! \033[0m"
